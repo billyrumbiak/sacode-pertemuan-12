1259874b60376ed3b1f21e53d6e58fa3dc906f64
Billy Rumbiak <rumbiakbilly1995@gmail.com>
2024-07-02 19:45:38 +0900

    commit pertama

--- main.js
+++
@@ -1,7 +0,0 @@
-// method menambahkan teks
-document.getElementById("demo").textContent = "Hello World";
-
-// method tambahkan teks dan tag
-const create = document.createElement("p");
-create.textContent = "Hello Billy";
-document.body.appendChild(create);
